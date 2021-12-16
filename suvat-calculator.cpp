907e93e83378285eda7bb64f6073a165a3a01130
Taisei Kikuta <0218kikutaisei@gmail.com>
2021-12-16 15:44:59 +0000

    Created initial files

---
+++ suvat-calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "\n\n\n\n\n"; // Delete this later! This is to add some spacing between executions!
+
+    
+
+    cout << "\n\n\n\n\n"; // Delete this later! This is to add some spacing between executions!
+    return 0;
+}
